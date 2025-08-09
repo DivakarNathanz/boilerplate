f58c2034019881963edeb36efa64da1095e29128
Divakar Nathan <d032210045@student.utem.edu.my>
2025-08-09 23:32:16 +0800

    First commit with edited files

---
+++ myApp.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
+app.use(helmet());
+const PORT = process.env.PORT || 5500;
+
+
+app.listen(PORT, () => {
+console.log(`Coolie Powerhouse uh ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
