a3ad7a2ce51a09318c06f432168ced3f777197ef
Divakar Nathan <d032210045@student.utem.edu.my>
2025-08-09 23:52:03 +0800

    1 port

--- myApp.js
+++ myApp.js
@@ -2,12 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 app.use(helmet());
-const PORT = process.env.PORT || 3030;
 
-
-app.listen(PORT, () => {
-console.log(`Coolie Powerhouse uh ${PORT}`);
-});
 
 
 
